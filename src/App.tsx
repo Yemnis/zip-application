1f71df407d5e4b811c27373ca8d6b43f2dbf8b0f
Yemeri <yemeri_b@hotmail.com>
2023-03-21 07:37:50 +0100

    New project
    Signed-off-by: Yemeri <yemeri_b@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
+
+interface File {
+  name: string;
+  data: string;
+}
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
+
+  const handleDrop = (acceptedFiles: File[]) => {
+    if (acceptedFiles.length > 0) {
+      const file = acceptedFiles[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        const fileData = reader.result as string;
+        setSelectedFile({ name: file.name, data: fileData });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleUpload = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/zip-file', { file: selectedFile?.data });
+      setDownloadUrl(URL.createObjectURL(new Blob([response.data])));
+      setError(null);
+    } catch (error) {
+      setError('An error occurred while uploading the file');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
 
 export default App;
+
