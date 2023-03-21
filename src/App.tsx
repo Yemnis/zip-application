1a1fe8804264231cf717a3c7a09a55bf112ad02a
Yemeri <yemeri_b@hotmail.com>
2023-03-21 08:44:11 +0100

    frontend part done
    Signed-off-by: Yemeri <yemeri_b@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,75 @@
-// Importing React, useState hook from react, Dropzone component from react-dropzone, and axios
 import React, { useState } from 'react';
-import Dropzone from 'react-dropzone';
+import Dropzone, { DropzoneState } from 'react-dropzone';
 import axios from 'axios';
 
-// Defining a File interface to hold the name and data of the uploaded file
-interface File {
+interface File extends Blob {
   name: string;
-  data: string;
+  data?: string;
+  lastModified?: number;
+  webkitRelativePath?: string;
 }
 
 function App() {
-  // Defining selectedFile, error and downloadUrl states using useState hook and initializing them with null
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [fileData, setFileData] = useState<string | null>(null);
+  const [fileName, setFileName] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
-
-  // handleDrop function takes an array of acceptedFiles as an argument
+  
   const handleDrop = (acceptedFiles: File[]) => {
-    // Checking if there is at least one file in the array
     if (acceptedFiles.length > 0) {
-      // Selecting the first file in the array
       const file = acceptedFiles[0];
-      // Creating a new FileReader object
       const reader = new FileReader();
-      // Setting onload function to be executed when reading is successful
       reader.onload = () => {
-        // Getting the result of reading as a string
         const fileData = reader.result as string;
-        // Storing the file name and data in the selectedFile state
-        setSelectedFile({ name: file.name, data: fileData });
+        setFileData(fileData);
+        setFileName(file.name);
       };
-      // Starting the reading process by calling readAsDataURL function with the selected file
-      reader.readAsDataURL(file);
+      reader.readAsBinaryString(file);
     }
   };
 
-  // handleUpload function handles the uploading and zipping of the file
   const handleUpload = async () => {
     try {
-      // Making a post request to the backend service with the selected file data
-      const response = await axios.post('http://localhost:3000/zip-file', { file: selectedFile?.data });
-      const blob = new Blob([selectedFile?.data], { type: 'application/zip' });
+      // Check if file data exists
+      if (!fileData) {
+        throw new Error('No file selected');
+      }
+
+      // Post file data to the server
+      const response = await axios.post('http://localhost:3000/zip-file', { file: fileData });
+
+      // Check if the response is valid
+      if (!response.data) {
+        throw new Error('Invalid response');
+      }
+
+      // Create blob from response data and set download URL
+      const blob = new Blob([response.data], { type: 'application/zip' });
       setDownloadUrl(URL.createObjectURL(blob));
-      // Clearing the error state if there was any
       setError(null);
     } catch (error) {
-      // Setting the error state if there was an error in uploading and zipping the file
       setError('An error occurred while uploading the file');
     }
   };
-  
 
-  // Rendering the Dropzone component and displaying the selected file name or a prompt to select a file
-  // The handleUpload function is called when the Upload and zip file button is clicked
-  // The error and downloadUrl states are displayed based on their respective conditions
   return (
     <div>
-      <Dropzone onDrop={handleDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <div {...getRootProps()}>
+      <div className='zip-container'>
+      <Dropzone onDrop={(acceptedFiles: File[]) => handleDrop(acceptedFiles)}>
+        {({ getRootProps, getInputProps, isDragActive }: DropzoneState) => (
+          <div {...getRootProps()} className={isDragActive ? "dropzone-active" : ""}>
             <input {...getInputProps()} />
-            {selectedFile ? (
-              <div>{selectedFile.name}</div>
+            {fileName ? (
+              <div>{fileName}</div>
             ) : (
               <div>Drag and drop or click here to select a file</div>
             )}
           </div>
         )}
       </Dropzone>
-      <button disabled={!selectedFile} onClick={handleUpload}>
+      </div>
+
+      <button disabled={!fileName} onClick={handleUpload}>
         Upload and zip file
       </button>
       {error && <div>{error}</div>}
@@ -84,5 +85,4 @@ function App() {
   );
 }
 
-// Exporting the App component as the default export
 export default App;
